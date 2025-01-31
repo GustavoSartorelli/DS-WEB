c23bbf738ea8909e4bcc15fbc7ee8e38660a649e
GustavoSartorelli <gustavosartorellima@gmail.com>
2025-01-31 15:13:52 -0300

    JavaScript

---
+++ Javascript/AtvJavascript/script.js
@@ -0,0 +1,42 @@
+console.log("Bom Dia, Mundo!!!")
+alert("Bom Dia, Mundo!!!")
+
+nome1 = prompt("Digite seu primeiro nome")
+nome2 = prompt("Digite seu sobrenome")
+alert(nome1 + " " + nome2)
+
+console.log("Digite a seguir numeros para fazer a adição")
+alert("Digite a seguir numeros para fazer a adição")
+var adicao1 = Number.parseInt(prompt("Digite o primeiro numero"))
+var adicao2 = Number.parseInt(prompt("Digite o segundo numero"))
+alert(adicao1 + adicao2)
+
+console.log("Digite a seguir numeros para fazer a subtração")
+alert("Digite a seguir numeros para fazer a subtração")
+var subtracao1 = Number.parseInt(prompt("Digite o primeiro numero"))
+var subtracao2 = Number.parseInt(prompt("Digite o segundo numero"))
+alert(subtracao1 - subtracao2)
+
+console.log("Digite a seguir numeros para fazer a multiplicação")
+alert("Digite a seguir numeros para fazer a multiplicação")
+var multiplicação1 = Number.parseInt(prompt("Digite o primeiro numero"))
+var multiplicação2 = Number.parseInt(prompt("Digite o segundo numero"))
+alert(multiplicação1 * multiplicação2)
+
+console.log("Digite a seguir numeros para fazer a divisão")
+alert("Digite a seguir numeros para fazer a divisão")
+var divisao1 = Number.parseInt(prompt("Digite o primeiro numero"))
+var divisao2 = Number.parseInt(prompt("Digite o segundo numero"))
+alert(divisao1 / divisao2)
+
+console.log("Digite a seguir numeros para fazer ao cubo")
+alert("Digite a seguir numeros para fazer ao cubo")
+var cubo1 = Number.parseInt(prompt("Digite o primeiro numero"))
+var cubo2 = Number.parseInt(prompt("Digite o segundo numero"))
+alert(cubo1 ** cubo2)
+
+console.log("Digite a seguir numeros para realizar a conversão de uma temperatura")
+alert("Digite a seguir numeros para realizar a conversão de uma temperatura")
+var pnumero = Number.parseInt(prompt("Digite a temperatura"))
+alert(pnumero - 32) = resultado
+alert(resultado * 0,5556)
