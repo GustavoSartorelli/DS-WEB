99263c3b3af85bfc29991a02c56f020f5a6db76c
GustavoSartorelli <gustavosartorellima@gmail.com>
2025-01-31 12:17:03 -0300

    Javascript

---
+++ Javascript/Aula-01/readme.md
@@ -0,0 +1,5 @@
+Aula Introdução sobre logica de Programação com Javascript
+
+Declaração de variaveis
+Alert,Confirm,Prompt
+Alguns comandos
---
+++ Javascript/Aula-01/script.js
@@ -0,0 +1,24 @@
+console.log("Ola Mundo");
+
+
+//Exemplo de alert()
+    alert("Ola Mundo 1");
+
+//Exemplo de confirm
+    var teste = confirm("Ola Mundo 2")
+    //Exibindo a opcao escolhida pelo usuario
+    console.log(teste)
+
+
+//Exemplo de Prompt()
+    var testePrompt = prompt("Digite seu nome")
+    //exibindo o nome do usuario
+    console.log(testePrompt)
+    alert(testePrompt)
+
+//Tentativa de Adição
+    var numero1 = Number.parseInt(prompt("Digite o primeiro numero"))
+    var numero2 = Number.parseInt(prompt("Digite o segundo numero"))
+    //Verificando o tipo de variavel
+    console.log(typeof(numero1))
+    alert(numero1 + numero2)
