16e07c4477caaf919efbba41677648734a1d151f
GustavoSartorelli <gustavosartorellima@gmail.com>
2025-01-31 16:00:14 -0300

    Atv1_finalizada

--- Javascript/AtvJavascript/script.js
+++ Javascript/AtvJavascript/script.js
@@ -1,10 +1,13 @@
+//1
 console.log("Bom Dia, Mundo!!!")
 alert("Bom Dia, Mundo!!!")
 
+//2
 nome1 = prompt("Digite seu primeiro nome")
 nome2 = prompt("Digite seu sobrenome")
 alert(nome1 + " " + nome2)
 
+//3
 console.log("Digite a seguir numeros para fazer a adição")
 alert("Digite a seguir numeros para fazer a adição")
 var adicao1 = Number.parseInt(prompt("Digite o primeiro numero"))
@@ -29,14 +32,24 @@ var divisao1 = Number.parseInt(prompt("Digite o primeiro numero"))
 var divisao2 = Number.parseInt(prompt("Digite o segundo numero"))
 alert(divisao1 / divisao2)
 
+//4
 console.log("Digite a seguir numeros para fazer ao cubo")
 alert("Digite a seguir numeros para fazer ao cubo")
 var cubo1 = Number.parseInt(prompt("Digite o primeiro numero"))
 var cubo2 = Number.parseInt(prompt("Digite o segundo numero"))
 alert(cubo1 ** cubo2)
 
+//5
 console.log("Digite a seguir numeros para realizar a conversão de uma temperatura")
 alert("Digite a seguir numeros para realizar a conversão de uma temperatura")
 var pnumero = Number.parseInt(prompt("Digite a temperatura"))
 alert(pnumero - 32) = resultado
 alert(resultado * 0,5556)
+
+//6
+var capital = Number.parseInt(prompt("Digite Capital inicial investido"))
+var taxa = prompt("Digite a taxa em porcentagem")
+var tempo = prompt("Digite o tempo investido em meses")
+
+var R = ((capital * (1+(taxa/100)) ** tempo))
+alert (R.toFixed(2))
