0e46713ae3b3d4832b8e25b7e9ab465e3d8f0009
GustavoSartorelli <gustavosartorellima@gmail.com>
2025-02-04 14:17:37 -0300

    Salvando

---
+++ PHP/P.O.O/index.php
@@ -0,0 +1,18 @@
+<?php
+
+class Veiculo{
+
+    //Atributos:
+    public $Marca;
+
+    protected $Seguro;
+
+    private $renavam;
+
+    //Metodo:
+    public function ligar(){
+        
+    }
+}
+
+?>
