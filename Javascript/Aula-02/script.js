01515380edd0aef921ec3b2b6c6ee9af49f5155f
GustavoSartorelli <gustavosartorellima@gmail.com>
2025-02-07 13:59:34 -0300

    Save aprendizado de DOM

--- Javascript/Aula-02/script.js
+++ Javascript/Aula-02/script.js
@@ -27,7 +27,13 @@ function calcular(){
     var numero1 = document.getElementById("numero1").value;
     var numero2 = document.getElementById("numero2").value;
 
+    //Convertendo numeros
+    console.log(typeof numero1);
+    var numero1 = Number.parseInt(numero1);
+    var numero2 = Number.parseInt(numero2);
+    console.log(typeof numero2);
+
     var resultado = numero1 + numero2;
 
-    document.getElementById("resultado").innerHTML = "resultado:  " + resultado;
+    document.getElementById("resultado").innerHTML = "resultado:  "+resultado;
 }
