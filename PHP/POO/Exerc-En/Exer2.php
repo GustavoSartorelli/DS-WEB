f761b26391ec9362a01efeb64e8e8a0f631cee1f
GustavoSartorelli <gustavosartorellima@gmail.com>
2025-02-18 13:43:19 -0300

    Save Exercicio 2

---
+++ PHP/POO/Exerc-En/Exer2.php
@@ -0,0 +1,51 @@
+<?php
+    abstract class Animal{
+        public function fazerSom(){}
+    }
+
+    class Cachorro extends Animal{
+        public function falar(){
+            echo "Au-Au";
+        }
+        public function mover(){
+            return "Anda de quatro patas";
+        }
+    }
+
+    class Gato extends Animal{
+        public function falar(){
+            echo "Miau";
+        }
+        public function mover(){
+            return "Anda de quatro patas";
+        }
+    }
+
+    class Passaro extends Animal{
+        public function falar(){
+            echo "Piu-Piu"; 
+        }
+
+        public function mover(){
+            return "Anda de duas patas";
+        }
+    }
+    $snoop = new Cachorro();
+
+    echo $snoop->falar() . "<br/>";
+    echo $snoop->mover() . "<br/>";
+
+    echo "-------------------------<br/>";
+
+    $frajola = new Gato();
+
+    echo $frajola->falar() . "<br/>";
+    echo $frajola->mover() . "<br/>";
+
+    echo "-------------------------<br/>";
+
+    $zeCarioca = new Passaro();
+
+    echo $zeCarioca->falar() . "<br/>";
+    echo $zeCarioca->mover() . "<br/>";
+?>
