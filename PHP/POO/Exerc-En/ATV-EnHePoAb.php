fb59bc16391d6f39abfa23b240f57019ddccbdaf
GustavoSartorelli <gustavosartorellima@gmail.com>
2025-02-18 07:59:31 -0300

    Save atv php 5 questões

--- PHP/POO/Exerc-En/ATV-EnHePoAb.php
+++ PHP/POO/Exerc-En/ATV-EnHePoAb.php
@@ -1,25 +1,43 @@
 <?php
 
 class Pessoa{
-
     public $nome = "Gustavo";
-    protected $idade = "17";
+    public $idade = "17";
 
     public function verDados(){
         echo $this->nome."<br>";
         echo $this->idade."<br>";
     }
 }
+$Dados = new Pessoa();
+$Dados->verDados();
 
 class Funcionario extends Pessoa{
 
-    protected $salario = "1234";
+    public $salario = "123456789";
 
-    protected function verDados($n){
+    public function verSalario(){
         echo $this->salario."<br>";
-        $this->salario=$n;
     }
 }
+$Salario = new Funcionario();
+$Salario->verSalario();
 
+class Gerente extends Funcionario{
 
+    public function calcularBonus(){
+        return "Gerente recebe 20% Desconto";
+    }
+}
+$gerente = new Gerente();
+echo $gerente->calcularBonus()."<br>";
+
+class Desenvolvedor extends Funcionario{
+
+   public function calcularbonus(){
+        return "Desenvolvedor recebe 10% Desconto";
+   }
+}
+$desenvolvedor = new Desenvolvedor();
+echo $desenvolvedor->calcularBonus()."<br>";
 ?>
