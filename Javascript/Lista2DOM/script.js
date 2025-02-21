cfdd873efb7fc48ee804a9871e0e9cc8fe64b4fc
GustavoSartorelli <gustavosartorellima@gmail.com>
2025-02-21 14:57:56 -0300

    Salve Atividade lista 2

--- Javascript/Lista2DOM/script.js
+++ Javascript/Lista2DOM/script.js
@@ -53,3 +53,50 @@
         }
         document.getElementById("resul").innerHTML = resultado;
     }
+
+    //Crie um programa que peça ao usuário para inserir um número e informe se é par ou ímpar.
+
+    function CalculoImparpar(){
+        var valor = document.getElementById("ImparPar").value;
+        var ImparPar = parseInt(valor);
+        var resultado = ImparPar % 2 === 0 ? "Par" : "Ímpar";
+        document.getElementById("resultadoImparPar").innerHTML = ImparPar + " é um numero " + resultado;
+    }
+
+    //Crie um programa que solicite ao usuário três números e informe qual deles é o maior.
+    function verificarMaior(){
+        var numero1 = Number(document.getElementById("PriNumero").value);
+        var numero2 = Number(document.getElementById("SegNumero").value);
+        var numero3 = Number(document.getElementById("TerNumero").value);
+
+        var resultado = "";
+
+        if(numero1 > numero2 && numero1 > numero3){
+            resultado = numero1;
+        }else if(numero2 > numero3 && numero2 > numero1){
+            resultado = numero2;
+        }else{
+            resultado = numero3;
+        }
+        document.getElementById("resultadoMaior").innerHTML = resultado;
+    }
+
+    //Crie um programa que peça ao usuário inserir os comprimentos de três lados de um triângulo e determine se é um triângulo válido
+    function calculoTri(){
+        var lado1 = Number(document.getElementById("lado1").value);
+        var lado2 = Number(document.getElementById("lado2").value);
+        var lado3 = Number(document.getElementById("lado3").value);
+
+    
+        if((lado1 + lado2) > lado3 && (lado1 + lado3) > lado2 && (lado2 + lado3) > lado1){
+           if(lado1 == lado2 && lado1 == lado3){
+            document.getElementById("resultadoTriangulo").innerText = "triangulo é equilatero"
+           }else if(lado1 == lado2 && lado1 != lado3){
+            document.getElementById("resultadoTriangulo").innerText = "triangulo é isoceles"
+           }else{
+            document.getElementById("resultadoTriangulo").innerText = "triangulo é escaleno"
+           }
+        }else{
+            document.getElementById("resultadoTriangulo").innerText = "triangulo é valido";
+        }
+    }
